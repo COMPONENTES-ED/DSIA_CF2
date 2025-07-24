f5af4bf1e248231b448165dae0a9f3f6dc61b8d1
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-07-24 17:39:36 -0500

    Actividad actualizada

--- public/actividades/story_content/triggers.js
+++ public/actividades/story_content/triggers.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6g6Sg7hfVkB":
+      case "6CxIRbL30T4":
         Script1();
         break;
   }
@@ -12,7 +12,15 @@ window.InitExecuteScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
 };
